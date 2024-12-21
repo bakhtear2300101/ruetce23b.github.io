71110260b5f4d11b7ae6d5036ef155bd39894134
bakhtear2300101 <smbakhtearmahbub@gmail.com>
2024-12-21 19:30:30 +0600

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -2,5 +2,5 @@
 
 document.addEventListener('DOMContentLoaded', function() {
     console.log('Document is ready.');
-    // You can add more interactivity here
+    // Add any interactivity here if needed
 });
